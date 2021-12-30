7fbf8ce5f3c02d09520e5bf2326603edd6915371
Stanley Masinde <stanmasinde@gmail.com>
2021-12-30 17:04:10 +0300

    Mobile resposiveness and copyright year

--- assets/main.js
+++ assets/main.js
@@ -7,24 +7,50 @@
  * @date 2021-12-23
  * @link
  */
+const sections = {
+    "home": {
+        "title": "Home | Stanley Masinde",
+    },
+    "skills-section": {
+        "title": "Skills | Stanley Masinde",
+    },
+    "about-section": {
+        "title": "About | Stanley Masinde",
+    },
+    "projects-section": {
+        "title": "Projects | Stanley Masinde",
+    },
+    "contact-section": {
+        "title": "Contact | Stanley Masinde",
+    },
+};
+
 window.addEventListener('load', function () {
     const sectionLinks = document.querySelectorAll('#section-link');
     const backToTop = document.querySelector('#back-to-top');
+    const copyrightYear = document.querySelector('#copyright-year');
+
+    // Set the copyright year
+    let currentYear = new Date().getFullYear();
+    copyrightYear.innerHTML = currentYear;
 
     sectionLinks.forEach(function (link) {
         link.addEventListener('click', function (event) {
-            console.log(event.target.dataset.section);
             event.preventDefault()
 
             const href = link.getAttribute('href');
             const section = document.querySelector(href);
+            sectionLinks.forEach(function (link) {
+                link.classList.remove('active');
+            });
+            link.classList.add('active');
 
             section.scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
             });
 
-            document.title = href.substring(1);
+            document.title = sections[href.substring(1)].title;
         })
     })
 
