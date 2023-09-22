fc2a4ddf7cdf5e48f768678570f383a526fdb53e
Stanley Masinde <stanmasinde@gmail.com>
2023-09-22 21:26:54 +0300

    chore: update the skills section

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const navigationDrawer = document.getElementById("navigationDrawer");
+const toggleSideBarButton = document.getElementById("toggleSideBarButton");
+const sideNavShade = document.getElementById("sideNavShade");
+const skillsSection = document.getElementById("skillsSection");
 
-setupCounter(document.querySelector('#counter'))
+toggleSideBarButton.addEventListener("click", (event) => {
+  navigationDrawer.classList.toggle("hidden");
+});
+sideNavShade.addEventListener("click", (event) => {
+  event.preventDefault();
+  navigationDrawer.classList.add("hidden");
+});
+
+const skills = [
+  {
+    title: "Backend development",
+    description:
+      "I've been building backend solutions for over 4 years. My main tools of trade are Node.js, Laravel, MySQL, Redis and more",
+  },
+  {
+    title: "Frontend development",
+    description:
+      "I build high performant frontend applications using Vue.Js, Nuxt and tailwind. My applications work both online and offline",
+  },
+  {
+    title: "Data engineering",
+    description:
+      "I have experience in building data collection architectures. From advanced SQL, graph databases and more. Data will always be ready for scientists",
+  },
+];
+
+skills.forEach((skill) => {
+  const childElement = document.createElement("div");
+  const title = document.createElement("h1");
+  const description = document.createElement("p");
+  title.innerText = skill.title;
+  description.innerText = skill.description;
+  childElement.appendChild(title);
+  childElement.appendChild(description);
+  skillsSection.appendChild(childElement);
+  childElement.classList.add("p-4");
+  title.classList.add("font-bold", "text-2xl");
+  console.log(skillsSection);
+});
