4c165f38e6b5db390f9f3813d9ab069c03d8cd8e
Stanley Masinde <stanmasinde@gmail.com>
2023-09-23 14:43:09 +0300

    chore: fixes
    - The copyright date is now okay
    - The emojis on the greeting
    - The projects section is now working

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const navigationDrawer = document.getElementById("navigationDrawer");
 const toggleSideBarButton = document.getElementById("toggleSideBarButton");
 const sideNavShade = document.getElementById("sideNavShade");
 const skillsSection = document.getElementById("skillsSection");
+const featuredProjectsSection = document.getElementById("featuredProjects");
+const copyrightSpan = document.getElementById('copyright')
 
 toggleSideBarButton.addEventListener("click", (event) => {
+    event.preventDefault()
   navigationDrawer.classList.toggle("hidden");
 });
 sideNavShade.addEventListener("click", (event) => {
@@ -42,5 +45,54 @@ skills.forEach((skill) => {
   skillsSection.appendChild(childElement);
   childElement.classList.add("p-4");
   title.classList.add("font-bold", "text-2xl");
-  console.log(skillsSection);
 });
+
+const featuredProjects = [
+  {
+    title: "Notes",
+    description:
+      "A simple notes application built using indexed DB and works fully offline. Inspired by Samsung notes",
+    sourceCode: "https://github.com/stanleymasinde/notes",
+    link: "https://notes-n.netlify.app/",
+  },
+  {
+    title: "Gumzo",
+    description:
+      "Gumzo is Swahili for conversation. A pointless chat application built on Firebase. No account needed dive in and chat with random strangers",
+    sourceCode: "https://github.com/stanleymasinde/gumzo",
+    link: "https://stanleymasinde.github.io/gumzo/",
+  },
+  {
+    title: "Mock API",
+    description:
+      "A tool that returns fake JSON data on endpoint calls. It is ideal for frontend testing",
+    sourceCode: "https://github.com/opensource254/mock-api",
+  },
+];
+
+featuredProjects.forEach((project) => {
+  const childElement = document.createElement("div");
+  childElement.innerHTML = `<div v-for="p in featuredProjects"
+          class="bg-primary flex-1 text-white shadow-md shadow-bgl border border-bgl mt-2 mb-3 rounded-3xl">
+          <div class="rounded-t-3xl h-48 bg-cover bg-no-repeat aspect-video w-full flex justify-center">
+            <div class="text-center grid place-content-center">
+              <h1 class="font-bold text-8xl">${project.title.charAt(0)}</h1>
+            </div>
+          </div>
+          <div class="px-3 mt-3">
+            <div>
+              <h1 class="text-2xl font-bold">${project.title}</h1>
+              <p class="">${project.description}</p>
+            </div>
+            <div class="flex justify-center gap-3 mt-2 py-4 rounded-lg">
+              <a class=" transition-all delay-150 border rounded-lg px-2 py-1 hover:bg-tertiary hover:border-tertiary"
+                :href="p.link" target="_blank" rel="noopener noreferrer">Live link</a>
+              <a class="border rounded-lg px-2 py-1 hover:bg-tertiary hover:border-tertiary" :href="p.sourceCode"
+                target="_blank" rel="noopener noreferrer">Source code</a>
+            </div>
+          </div>
+        </div>`;
+  featuredProjectsSection.appendChild(childElement);
+});
+
+copyrightSpan.innerText = new Date().getFullYear()
