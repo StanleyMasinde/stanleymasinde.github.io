aeb0ee544981d3a0b0b2b97ba7fdf8080abf8f1f
Stanley Masinde <stanmasinde@gmail.com>
2025-06-21 15:40:43 +0300

    add the landing page markup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+	compatibilityDate: '2025-05-15',
+	devtools: { enabled: true },
+	modules: ['@nuxt/eslint'],
+
+	css: ['~/assets/css/main.css'],
+
+	vite: {
+		plugins: [
+			tailwindcss(),
+		]
+	}
 })
