529f11c27973abae6e918de68173524bdbc672d9
Stanley Masinde <stanmasinde@gmail.com>
2025-06-23 17:13:58 +0300

    remove old sw

--- plugins/pwa.client.ts
+++ plugins/pwa.client.ts
@@ -2,6 +2,6 @@ export default defineNuxtPlugin(async () => {
 	console.log('Removing old serrvice workers for the transitional period')
 	if (navigator.serviceWorker) {
 		const registrations = await navigator.serviceWorker.getRegistrations()
-		Promise.all(registrations.map(reg => reg.unregister()))
+		await Promise.all(registrations.map(reg => reg.unregister()))
 	}
 })
